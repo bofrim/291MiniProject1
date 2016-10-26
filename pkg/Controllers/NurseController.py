137c8207101f0d8412f8476605f1b6d8ea789c5c
Mark Griffith <mgriffit@ualberta.ca>
2016-10-26 17:05:34 -0600

    Nurse Controller needs getCursor to work:/

--- pkg/Controllers/NurseController.py
+++ pkg/Controllers/NurseController.py
@@ -1,69 +1,125 @@
 from CareStaffController import CareStaff
-from LoginContoller import Login
+from LoginController import *
 
 class Nurse(CareStaff):
 
+    def __init__(self, staffID):
+        self.staffID = staffID
+        self.main()
+
+    def main(self):
+        # self.staffID = staffID
+        c = Login.getCursor()
+        user = ""
+        print("Create a new chart for a patient - 'create'\n")
+        print("Close an open chart for a patient - 'close'\n")
+        print("add a symptom to a patient's chart - 'S'\n")
+        print("List all charts for a given patient - 'charts'\n")
+        print("Logout - 'exit'\n")
+        l = ["create", "close", "S", "charts"]
+
+        while user != "exit":
+            user = raw_input("What would you like to do?\n")
+            if user in l:
+                patientHcno = raw_input("Patient Healthcare Number: ")
+                chartID = getChartID(patientHcno)
+                if user == "create":
+                    if chartID is not None:
+                        choice = raw_input("There is already a chart open for this patient."
+                                            "Would you like to create a new chart? (y/n): ")
+                        if choice is "n":
+                            continue
+                        else:
+                            closeChart(c, patientHcno)
+                            createChart(c, patientHcno)
+
+                elif user == "close":
+                    if chartID is None:
+                        print('There is no chart open for that patient')
+                        continue
+                    closeChart(c, patientHcno)
+
+                elif user == "S":
+                    symptom = getSymptom()
+                    if chartID == None:
+                        print('There is no chart open for that patient')
+                        continue
+                    addSymptom(c, patientHcno, chartID, self.staffID, symptom)
+
+                elif user == "charts":
+                    if chartID is None:
+                        print('There is no chart open for that patient')
+                        continue
+                    getPatientCharts(c, patientHcno)
+
+        print('Logging out... Goodbye!')
+
     def createChart(c, patientHcno):
+        c.execute
         newChartId = getNewChartID(c)
-        Login.getCursor().execute(
+        c.execute(
             '''
             INSERT INTO charts VALUES(?,?,date('now'), ?);
             ''', patientChartId, patientHcno, (None,))
-        Login.commit()
+        commit()
 
     def closeChart(c, patientHcno):
         patientsOpenChart = getMostRecentChart(c, patientHcno)
-        Login.getCursor().execute(
+        c.execute(
             '''
             UPDATE charts SET edate = date('now') WHERE charId = ?;
             ''', patientChartId)
-        Login.commit()
+        commit()
 
     def getNewChartId(c):
-        Login.getCursor().execute(
+        c.execute(
             '''
             SECLECT COUNT(*) FROM charts;
             '''
         )
-        row = Login.getCursor().fetchOne()
+        row = c.fetchOne()
         newId = row[0] + 1
         return format(newId, '05') #will left pad w/ zeros up to 5 digets
 
     def getMostRecentChart(c, patientHcno):
-        Login.getCursor().execute(
+        c.execute(
             '''
             SELECT chart_id
             FROM charts
             WHERE hcno = patientChartID
-            ORDER BY adate
+            ORDER BY adate desc;
             '''
         )
-        row = Login.getCursor().fetchOne()
+        row = c.fetchOne()
         return row[0]
 
     # not sure if this works yet
     def hasChartOpen(c, patientChartId):
-        Login.getCursor().execute(
+        c.execute(
             '''
             SELECT edate
             FROM charts
             WHERE hcno = ?
-            ORDER BY adate
+            ORDER BY adate;
             ''',patientChartId
         )
-        row = Login.getCursor().fetchOne()
+        row = c.fetchOne()
         return row[1] != None # false if None
 
+    def getChartID(c, hcno):
+        c.execute('SELECT chart_id FROM charts WHERE hcno=:hcno AND edate <> NULL;', {"hcno": hcno})
+        chart_id = c.fetchone()
+        if chart_id is not None:
+            return chart_id[0]
+        else: return None
 
-    options = {
-    # "getPatientCharts" : ("Get a list of all of a specified patient's charts. Then select a chart to view.", getPatientCharts),
-    # "addSymptom" : ("Add a symptom to the chart of a specified patient.", addSymptom),
-    "createChart" : ("Create a new chart for a specified patient.", createChart),
-    "closeChart" : ("Close the specified patient's open chart.", closeChart)
-    }
-
+    def commit():
+        conn.commit()
 #______________________________________________________Views_________
     @staticmethod
     def override():
         return raw_input("There is already an open chart for this patient."
                         "Would you like to close it and open a new chart? (y/n): ")
+    @staticmethod
+    def getSymptom():
+        return raw_input("Reported Symptom: ")
